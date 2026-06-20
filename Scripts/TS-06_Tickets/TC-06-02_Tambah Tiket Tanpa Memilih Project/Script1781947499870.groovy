90bcba0a8e242746d579d41b08b608374d28ec44
Arnora <ardora.334@gmail.com>
2026-06-20 16:37:28 +0700

    fix : TC-06-02 memperbaiki script validasi

--- Scripts/TS-06_Tickets/TC-06-02_Tambah Tiket Tanpa Memilih Project/Script1781947499870.groovy
+++ Scripts/TS-06_Tickets/TC-06-02_Tambah Tiket Tanpa Memilih Project/Script1781947499870.groovy
@@ -38,5 +38,5 @@ WebUI.setText(findTestObject('Tickets/Page_- GiPM/textarea_border rounded px-3 p
 
 WebUI.click(findTestObject('Tickets/Page_- GiPM/button_Tambah Tiket'))
 
-WebUI.verifyTextPresent('Penambahan Fitur Lupa Kata  Sandi', false)
+WebUI.verifyTextNotPresent('Penambahan Fitur Lupa Kata  Sandi', false)
 
